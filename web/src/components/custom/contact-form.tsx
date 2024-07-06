b51e1be676432f867723bc7ee3a49129cea545a5
siddheshjungade007 <siddheshshaym.j@digitalapicraft.com>
2024-07-06 15:59:06 +0530

    added working contact form
    Signed-off-by: siddheshjungade007 <siddheshshaym.j@digitalapicraft.com>

--- web/src/components/custom/contact-form.tsx
+++ web/src/components/custom/contact-form.tsx
@@ -1,11 +1,16 @@
 "use client";
 import { Input } from "../ui/input";
 import { Button } from "@/components/ui/button";
-import { toast, useToast } from "@/components/ui/use-toast";
+import { useToast } from "@/components/ui/use-toast";
 import { Textarea } from "../ui/textarea";
 import { DevImg } from "./dev-img";
 import Image from "next/image";
 import { PersonalInfo } from "./personal-info";
+import { FormEvent, useState } from "react";
+
+const { Client } = require("@notionhq/client");
+
+const notion = new Client({ auth: process.env.NOTION_API_KEY });
 
 const inputFieldList = [
   {
@@ -21,19 +26,51 @@ const inputFieldList = [
   {
     name: "Mobile No",
     type: "text",
-    pattern: "^d{5}(-d{4})?$",
     placeholder: "Mobile No",
   },
 ];
 
 export const ContactForm = () => {
-  // const { toast } = useToast();
-
-  const username = process.env.NEXT_PUBLIC_CALENDLY_USERNAME;
-  const showDetails: number =
-    process.env.NEXT_PUBLIC_CALENDLY_SHOW_DETAILS == "false" ? 1 : 0;
-  const showCookies: number =
-    process.env.NEXT_PUBLIC_CALENDLY_SHOW_COOKIES == "false" ? 1 : 0;
+  const { toast } = useToast();
+  const [contactObj, setContactObj] = useState<any>({});
+  const contactFormHandler = async (e: FormEvent) => {
+    try {
+      e.preventDefault();
+      console.log(contactObj)
+      const body: any = {
+        name: contactObj['Name'],
+        email: contactObj["Email"],
+        message: contactObj["message"],
+        mobile: contactObj["Mobile No"],
+        source: "portfolio"
+      }
+      console.log(body)
+      const respone = await fetch(
+        "https://api-goconsistent-tech.onrender.com/notion/contact",
+        {
+          headers: {
+            "Content-Type": "application/json"
+          },
+          method: "POST",
+          body: JSON.stringify(body),
+        }
+      );
+      console.log(respone)
+      if(respone.ok){
+        toast({
+          title: "Thank you for connecting",
+          description: "I will get back to shortly",
+        });
+        setContactObj({});
+      }
+    } catch (e) {
+      toast({
+        variant: "destructive",
+        title: "Error",
+        description: JSON.stringify(e),
+      });
+    }
+  };
 
   return (
     <>
@@ -41,31 +78,31 @@ export const ContactForm = () => {
         <div className="hidden lg:block w-[600px] h-full">
           <PersonalInfo />
         </div>
-        <form className="w-[90%] h-full md:w-[40em] mt-3 flex flex-col justify-stretch gap-4">
+        <form
+          className="w-[90%] h-full md:w-[40em] mt-3 flex flex-col justify-stretch gap-4"
+          onSubmit={(e: FormEvent) => contactFormHandler(e)}
+        >
           <h1 className="font-bold text-4xl text-center"> Drop message </h1>
           {inputFieldList.map((field, idx) => (
             <Input
               key={field.name}
               type={field.type}
+              value={contactObj[field.name] ? contactObj[field.name] : ""}
               placeholder={field.placeholder}
-              pattern={field?.pattern}
+              onChange={(e) =>
+                setContactObj({ ...contactObj, [field.name]: e.target.value })
+              }
             />
           ))}
           <Textarea
             className="h-[300px] xl:h-[120px] justify-self-stretch self-stretch"
             placeholder="Type your message here."
+            value={contactObj["message"] ? contactObj["message"] : ""}
+            onChange={(e) =>
+              setContactObj({ ...contactObj, message: e.target.value })
+            }
           />
-          <Button
-            className="w-full"
-            onClick={() => {
-              toast({
-                title: "Thank you",
-                description: "I will contact u shortly",
-              });
-            }}
-          >
-            Submit
-          </Button>
+          <Button className="w-full">Submit</Button>
         </form>
       </div>
       {/* <div
